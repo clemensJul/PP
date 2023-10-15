6d30133694446a135f65401df124441154ff282e
Raphael Wirnsberger <rwirnsberger@anexia-it.com>
2023-10-15 17:27:36 +0200

    add comments to Nest

--- Aufgabe1-3/Nest.java
+++ Aufgabe1-3/Nest.java
@@ -3,14 +3,29 @@ import java.util.LinkedList;
 
 
 public class Nest extends Tile {
-
+    /**
+     * Initializes the Nest at the given position
+     *
+     * @param  position Position where Nest is located.
+     */
     public Nest(Vector position) {
         super(position);
     }
+
+    /**
+     * Returns the Color of Nest.
+     *
+     * @return {@link Color#YELLOW}
+     */
     public Color getTileColor(){
         return Color.YELLOW;
     }
 
+    /**
+     * Nest needs no updates because it does not change colors.
+     *
+     * @return true
+     */
     @Override
     public boolean update() {
         return true;

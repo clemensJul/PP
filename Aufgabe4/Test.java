9f1dd2b7b7b29ae5e8b9be89b234d97ebd1306ff
Raphael Wirnsberger <rwirnsberger@anexia-it.com>
2023-11-08 13:44:11 +0100

    add test definitions for types

--- Aufgabe4/Test.java
+++ Aufgabe4/Test.java
@@ -1,5 +1,36 @@
 public class Test {
     public static void main(String[] args) {
         ESubstrat s = ESubstrat.TEST;
+
+        // compositeFormicarium
+        FormicariumItem fi_compositeFormicarium = new CompositeFormicarium();
+        FormicariumPart fp_compositeFormicarium = new CompositeFormicarium();
+        Formicarium f_compositeFormicarium = new CompositeFormicarium();
+
+        // formicarium
+        FormicariumItem fi_formicarium = new Formicarium();
+        FormicariumPart fp_formicarium = new Formicarium();
+
+        // nest
+        FormicariumPart fp_nest = new Nest();
+        FormicariumItem fi_nest = new Nest();
+
+        // antfarm
+        FormicariumPart fp_antFarm = new AntFarm();
+        FormicariumItem fi_antFarm = new AntFarm();
+
+        // arena
+        FormicariumPart fp_arena = new Arena();
+        FormicariumItem fi_arena = new Arena();
+
+        // thermometer
+        CompatibleInstrument ci_thermometer = new Thermometer();
+        Instrument i_thermometer = new Thermometer();
+        FormicariumPart fp_thermometer = new Thermometer();
+        FormicariumItem fi_thermometer = new Thermometer();
+
+        // forceps
+        FormicariumItem fi_forceps = new Forceps();
+        Instrument i_forceps = new Forceps();
     }
 }
